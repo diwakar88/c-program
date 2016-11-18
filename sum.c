c5e50a85e4a143f96b24c5e0acd01541eae54313
Diwakar kandel <damodarkandel435@gmail.com>
2016-11-18 12:29:44 +0545

    Add files via upload
    WAP to print sum of two nos asked from user.

---
+++ sum.c
@@ -0,0 +1,21 @@
+ /*
+Student Name:Diwakar kandel
+Subject:programming fundamental
+Roll No:
+Lab No:01
+Program:WAP to print sum of two nos asked from user.
+Date:Nov18,2016
+*/
+#include<stdio.h>
+ int main()
+ {
+ int a,b,sum;
+ printf("Enter first nos:\n");
+ scanf("%d",& a);
+  printf("Enter second nos:\n");
+ scanf("%d",& b);
+ sum=a+b;
+ printf("sum of two nos of %d",sum);
+ return 0;
+ }
+ 
